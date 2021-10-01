e33c79f1a5a02ad3dab0c31cc240adfc7ca5c607
Simon Lim <simonlim898@hotmail.com>
2021-10-01 01:04:08 -0400

    Cleaned up existing code by adding logger, handling error, and adding right function params and arguments #44

--- backend/src/main/config/sequelize.ts
+++ backend/src/main/config/sequelize.ts
@@ -1,21 +1,22 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize-typescript';
 import { Account } from '../models/Account';
+import { ClientAccount } from '../models/ClientAccount';
 import { Event } from '../models/Event';
 import { Invited } from '../models/Invited';
+import { Project } from '../models/Project';
 
 dotenv.config();
-
 export const sequelize = new Sequelize({
-    database: "badob_tech",
-    dialect: 'mysql',
-    host: process.env.DB_HOSTNAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    storage: ':memory:',
-    models: [Account, Event, Invited],
-    define:{
-        freezeTableName:true
-    },
-    logging: false
-})
+  database: 'badob_tech',
+  dialect: 'mysql',
+  host: process.env.DB_HOSTNAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  storage: ':memory:',
+  models: [Account, Event, Invited, ClientAccount, Project],
+  define: {
+    freezeTableName: true,
+  },
+  logging: process.env.DEBUG ? true : false,
+});
