85edc01fb768d22e9f18ca2718532236166717c0
Simon Lim <simonlim898@hotmail.com>
2021-10-02 14:33:25 -0400

    Implemented Social media page orm #44

--- backend/src/main/config/sequelize.ts
+++ backend/src/main/config/sequelize.ts
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize-typescript';
 import { Account } from '../models/Account';
 import { AdminAccount } from '../models/AdminAccount';
+import { BusinessAccount } from '../models/BusinessAccount';
 import { ClientAccount } from '../models/ClientAccount';
 import { EmployeeAccount } from '../models/EmployeeAccount';
 import { Event } from '../models/Event';
 import { Invited } from '../models/Invited';
 import { Project } from '../models/Project';
+import { SocialMediaPage } from '../models/SocialMediaPage';
 
 dotenv.config();
 export const sequelize = new Sequelize({
@@ -16,7 +18,7 @@ export const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   storage: ':memory:',
-  models: [Account, Event, Invited, ClientAccount, Project, EmployeeAccount, AdminAccount],
+  models: [Account, Event, Invited, ClientAccount, Project, EmployeeAccount, AdminAccount, BusinessAccount, SocialMediaPage],
   define: {
     freezeTableName: true,
   },
