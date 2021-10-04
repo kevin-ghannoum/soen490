15a9a4c295e2f7b0fbb482812786a991d7b481fc
Lauren Lim <mnxnhl@gmail.com>
2021-10-04 14:30:33 -0400

    Adding models to sequelize config file #43

--- backend/src/main/config/sequelize.ts
+++ backend/src/main/config/sequelize.ts
@@ -1,21 +1,42 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize-typescript';
 import { Account } from '../models/Account';
+import { Business } from '../models/Business';
+import { EmailTemplate } from '../models/EmailTemplate';
 import { Event } from '../models/Event';
+import { Expense } from '../models/Expense';
+import { Goal } from '../models/Goal';
 import { Invited } from '../models/Invited';
+import { Invoice } from '../models/Invoice';
+import { Production } from '../models/Production';
+import { Project } from '../models/Project';
+import { Sale } from '../models/Sale';
+import { Transaction } from '../models/Transaction';
 
 dotenv.config();
 
 export const sequelize = new Sequelize({
-    database: "badob_tech",
-    dialect: 'mysql',
-    host: process.env.DB_HOSTNAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    storage: ':memory:',
-    models: [Account, Event, Invited],
-    define:{
-        freezeTableName:true
-    },
-    logging: false
-})
+  database: 'badob_tech',
+  dialect: 'mysql',
+  host: process.env.DB_HOSTNAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  models: [
+    Account,
+    Event,
+    Invited,
+    Business,
+    EmailTemplate,
+    Goal,
+    Sale,
+    Invoice,
+    Production,
+    Expense,
+    Transaction,
+    Project,
+  ],
+  define: {
+    freezeTableName: true,
+  },
+  logging: true,
+});
