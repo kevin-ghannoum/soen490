ebb2583f9fc4a7fcc2d6159c4b034db53c2a7886
Lauren Lim <mnxnhl@gmail.com>
2021-10-04 14:30:33 -0400

    Adding models to sequelize config file #43

--- backend/src/main/config/config.js
+++ backend/src/main/config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-      "username": "root",
-      "password": 'mysqlpw',
-      "database": "badob_tech",
-      "host": "localhost",
-      "dialect": "mysql"
+  development: {
+    username: 'root',
+    password: 'mysqlpw',
+    database: 'badob_tech',
+    host: 'localhost',
+    dialect: 'mysql',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "localhost",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: 'localhost',
+    dialect: 'mysql',
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "localhost",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+};
--- backend/src/main/config/sequelize.ts
+++ backend/src/main/config/sequelize.ts
@@ -6,6 +6,8 @@ import { BusinessAccount } from '../models/BusinessAccount';
 import { ClientAccount } from '../models/ClientAccount';
 import { EmployeeAccount } from '../models/EmployeeAccount';
 import { Event } from '../models/Event';
+import { Expense } from '../models/Expense';
+import { Goal } from '../models/Goal';
 import { Invited } from '../models/Invited';
 import { Pay } from '../models/Pay';
 import { Project } from '../models/Project';
@@ -14,6 +16,12 @@ import { Call } from '../models/Call';
 import { Address } from '../models/Address';
 import { SocialMediaPage } from '../models/SocialMediaPage';
 import { WorksOn } from '../models/WorksOn';
+import { Invoice } from '../models/Invoice';
+import { Production } from '../models/Production';
+import { Sale } from '../models/Sale';
+import { Transaction } from '../models/Transaction';
+import { EmailTemplate } from '../models/EmailTemplate';
+import { Business } from '../models/Business';
 
 dotenv.config();
 export const sequelize = new Sequelize({
@@ -22,7 +30,6 @@ export const sequelize = new Sequelize({
   host: process.env.DB_HOSTNAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  storage: ':memory:',
   models: [
     Account,
     Event,
@@ -37,7 +44,16 @@ export const sequelize = new Sequelize({
     BusinessAccount,
     SocialMediaPage,
     Pay,
-    WorksOn
+    WorksOn,
+    Business,
+    EmailTemplate,
+    Goal,
+    Sale,
+    Invoice,
+    Production,
+    Expense,
+    Transaction,
+    Project,
   ],
   define: {
     freezeTableName: true,
