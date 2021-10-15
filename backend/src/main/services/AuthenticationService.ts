0fea79d5e05e337ce75f8599854b83cff994c035
Manraj Rai <manraj14@icloud.com>
2021-10-15 19:49:13 -0400

    Testing authentication

--- backend/src/main/services/AuthenticationService.ts
+++ backend/src/main/services/AuthenticationService.ts
@@ -1,4 +1,4 @@
-import { DatabaseAuthenticator, SignInOptions } from 'auth0';
+import { AuthenticationClient } from 'auth0';
 import debug from 'debug';
 import { inject, injectable } from 'tsyringe';
 
@@ -7,7 +7,7 @@ const log: debug.IDebugger = debug('app:AuthenticationService');
 @injectable()
 export class AuthenticationService {
     constructor(
-        @inject('auth0-database-authenticator') private databaseAuthenticator: DatabaseAuthenticator
+        @inject('auth0-authentication-client') private authenticationClient: AuthenticationClient
     ) {
         log('Created instance of AuthenticationService');
     }
@@ -18,14 +18,14 @@ export class AuthenticationService {
     }) => {
         const data = {
             ...accountInfo,
-            //client_id: process.env.AUTH0_CLIENT_ID,
-            connection: process.env.AUTH0_CONNECTION,
-            //scope: 'openid',
+            client_id: process.env.AUTH0_CLIENT_ID,
+            realm: process.env.AUTH0_CONNECTION,
+            scope: 'openid',
             //otp: ''
         }
 
 
-        const token = await this.databaseAuthenticator.signIn(data as SignInOptions)
+        const token = await this.authenticationClient.passwordGrant(data)
         console.log(token)
 
         return token;
