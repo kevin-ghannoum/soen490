6f62d18838daaab2fd0f9b20ba9abbf9cbfd3f32
Judy Mezaber <55299067+jmezaber@users.noreply.github.com>
2021-10-16 18:44:24 -0400

    [MAJOR] Implemented frontend of the login page. (#54)
    * created frontend of the login page
    
    * Removed useState from Login.tsx
    
    * Formated Login.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { ThemeProvider } from '@material-ui/core';
 import { mainTheme } from './configs/MuiConfig';
+import Login from './components/Login/Login';
 
 const App = () => {
   return (
     <ThemeProvider theme={mainTheme}>
       <div className="App">
-        <div>First Page</div>
+        <Login />
       </div>
     </ThemeProvider>
   );
