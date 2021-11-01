4df826df5c610e7664b2985bc50a0cdb60cd0269
Simon Lim <simonlim898@hotmail.com>
2021-10-31 20:43:21 -0400

    Implemented frontend for create client account #32

--- frontend/src/components/CreateClientAccount/CreateClientFormValidationSchema.ts
+++ frontend/src/components/CreateClientAccount/CreateClientFormValidationSchema.ts
@@ -1,21 +1,24 @@
 import * as Yup from 'yup';
 
-const createEmployeeSchema: Yup.ObjectSchema<any> = Yup.object().shape({
+const createClientAccountSchema: Yup.ObjectSchema<any> = Yup.object().shape({
   firstName: Yup.string().required('Required'),
   lastName: Yup.string().required('Required'),
   email: Yup.string().email('Invalid email').required('Required'),
   username: Yup.string().required('Required'),
   password: Yup.string().required('Required'),
-  supervisorEmail: Yup.string().required('Required'),
   phone: Yup.string().required('Required'),
-  civicNumber: Yup.number().nullable().required('Required').typeError('Civic number must be a number'),
+  civicNumber: Yup.number().nullable().required('Required').typeError("Civic number must be a number"),
   postalCode: Yup.string().required('Required'),
   streetName: Yup.string().required('Required'),
   cityName: Yup.string().required('Required'),
   province: Yup.string().required('Required'),
   country: Yup.string().required('Required'),
-  title: Yup.string().required('Required'),
-  hourlyWage: Yup.number().nullable().required('Required').typeError('Hourly wage must be a number'),
+  businessName: Yup.string().required('Required'),
+  industry: Yup.string().required('Required'),
+  website: Yup.string().optional(),
+  socialMediaName: Yup.string().optional(),
+  socialMediaLink: Yup.string().optional(),
+  status:Yup.string().required('Required')
 });
 
-export default createEmployeeSchema;
+export default createClientAccountSchema;
