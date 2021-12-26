8044f1a3b2b9cd211b2b286f11194d002b9da8a7
Manraj Rai <48056716+Manraj45@users.noreply.github.com>
2021-12-26 14:40:37 -0500

    [MINOR] Added restriction on the endpoints in the backend.

--- frontend/cypress/integration/Task_Management/createTask.spec.js
+++ frontend/cypress/integration/Task_Management/createTask.spec.js
@@ -46,7 +46,7 @@ it('Should create a new Task', () => {
   cy.visit('/tasks/new');
   cy.get('input[name=title]').type("Testing Title");
   cy.get('textarea[name=description]').type("Testing Description");
-  cy.get('input[name=deadlineDate]').type('2021-11-13');
+  cy.get('input[name=deadlineDate]').type(new Date().toISOString().split('T')[0]);
   cy.get('#projectId-select').type("{enter}");
   cy.get('#mui-component-select-employees').type("{enter}{downarrow}{enter}");
 
