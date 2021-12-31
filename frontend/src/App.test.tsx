2979a3fae0bc98558e10bdbd33e9a9150f7f3644
kevin-ghannoum <kevin.ghannoum@gmail.com>
2021-12-30 21:35:18 -0500

    Rebasing

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import App from './App';
-
 test('initial test to test the framework', () => {
   expect(1).toBe(1);
 });
+
+export { };
