6e7009ed2c5365e65a8d021ae327699833067a0a
Simon <44126831+nasaku898@users.noreply.github.com>
2022-01-17 15:16:43 -0500

    [MINOR] Implemented error page UI (#178)

--- frontend/src/components/Shared/PageNotFound.tsx
+++ frontend/src/components/Shared/PageNotFound.tsx
@@ -1,9 +1,34 @@
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 
-const PageNotFound = () => {
+const PageNotFound: React.FC = () => {
   return (
-    <div>
-      <Typography>Error 404</Typography>
+    <div
+      style={{
+        backgroundColor: '#7ED0F0',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>
+        <img
+          src="https://cdn.discordapp.com/attachments/885685523954401340/931384157764984842/404_Pin.png"
+          width="50%"
+          alt="404 error"
+        ></img>
+        <Typography variant="h3" style={{ color: 'white', margin: '30px' }}>
+          We couldn't spark your page!
+        </Typography>
+        <a href="/" style={{ textDecoration: 'None' }}>
+          <Button
+            variant="contained"
+            style={{ padding: '10px', paddingLeft: '15px', paddingRight: '15px', background: 'white', margin: '20px' }}
+          >
+            <Typography style={{ color: '#2BB1E4', fontWeight: 'bold' }}>GO TO HOMEPAGE</Typography>
+          </Button>
+        </a>
+      </div>
     </div>
   );
 };
