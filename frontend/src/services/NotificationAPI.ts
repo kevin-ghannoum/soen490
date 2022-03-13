751e02aae3e11d048e4e83fc1bbb57197a61385b
bowsong <song.bowen1@gmail.com>
2022-03-13 16:30:54 -0400

    Implement basic notification getters
    Prettify notificaitons

--- frontend/src/services/NotificationAPI.ts
+++ frontend/src/services/NotificationAPI.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
 export const getAllNotificationsByCurrentUser = async (): Promise<AxiosResponse<any>> => {
-  return axios.get(`/notifications`);
+    return axios.get(`/notifications`);
 };
+  
