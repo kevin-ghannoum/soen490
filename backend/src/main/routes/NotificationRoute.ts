8fe4842da3619aaa69d8182fc5b50c370d83ba8c
bowsong <43589884+bowsong@users.noreply.github.com>
2022-03-16 22:18:02 -0400

    [MINOR] Implement getter for Notifications (#224)
    * Implement basic notification getter
    
    * Run prettier

---
+++ backend/src/main/routes/NotificationRoute.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { inject, injectable } from 'tsyringe';
+import { checkJwt } from '../middleware/JWTMiddleware';
+import { NotificationService } from '../services/NotificationService';
+import { getCurrentUserEmail } from '../utils/UserUtils';
+import { CommonRoutesConfig } from './CommonRoutesConfig';
+
+@injectable()
+export default class NotificationRoute extends CommonRoutesConfig {
+  constructor(@inject('express-app') app: express.Application, private notificationService: NotificationService) {
+    super(app, 'NotificationRoute');
+  }
+
+  private getCurrentUser = (req: express.Request): string => {
+    const authorizationToken: string = req.headers['authorization'] as string;
+    const id_token: string = authorizationToken.split(' ')[1];
+    return getCurrentUserEmail(id_token);
+  };
+
+  configureRoutes(): express.Application {
+    this.getApp()
+      .route(`/notifications`)
+      .all(checkJwt)
+      .get(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        try {
+          const currentUser: string = this.getCurrentUser(req);
+
+          const notifications = await this.notificationService.getAllByEmail(currentUser);
+          res.status(StatusCodes.OK).send(notifications);
+        } catch (err) {
+          next(err);
+        }
+      });
+
+    return this.getApp();
+  }
+}
---
+++ backend/src/main/services/NotificationService.ts
@@ -0,0 +1,16 @@
+import { injectable } from 'tsyringe';
+import debug from 'debug';
+import { Notification } from '../models/Notification';
+import NotificationRepository from '../repositories/NotificationRepository';
+const log: debug.IDebugger = debug('app:notificationService');
+
+@injectable()
+export class NotificationService {
+  constructor(private notificationRepository: NotificationRepository) {
+    log('Created new instance of NotificationService');
+  }
+
+  public getAllByEmail = async (email: string): Promise<Notification[] | null> => {
+    return this.notificationRepository.getAllByEmail(email);
+  };
+}
