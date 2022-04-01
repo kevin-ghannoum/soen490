94fe35a6006db2aa476a46df5e1ea62a082391b2
Simon Lim <simonlim898@hotmail.com>
2022-03-31 20:40:26 -0400

    fix edit business e2e test

--- frontend/cypress/integration/editBusiness.spec.js
+++ frontend/cypress/integration/editBusiness.spec.js
@@ -14,22 +14,20 @@ describe('EditBusinessAccount feature e2e test', () => {
 
   // Test user story: #208 As an admin, I want to manage businesses
   it('Should edit a business', () => {
-    cy.wait(500);
     getAllBusinessesIntercept();
     updateBusinessIntercept();
     getBusinessByIdIntercept();
 
     cy.visit('/business');
+    cy.wait('@getListOfBusinessesAPI')
 
     cy.get('#View-Business-Grid').should('exist');
-    cy.wait(500);
     cy.get(`[data-id="1"] > .MuiDataGrid-cell--withRenderer > .MuiSvgIcon-root`).click();
-    cy.wait(500);
     cy.get('input[name=website]').type('website link');
 
     cy.get('form').submit();
 
     cy.wait('@editBusinessAPI').its('response.statusCode').should('eq', 200);
-
+    cy.wait('@getBusinessAPI')
   });
 });
